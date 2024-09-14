7fbe8b9eeb773b230d8cb3610a07d48030a446eb
Atul1307 <katul2015@gmail.com>
2024-09-14 23:16:15 +0530

    Hides scrollbar while keeping scroll behaviour

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,7 +4,7 @@ import MovieCard from './MovieCard';
 const MovieList = ({ title, movies }) => {
   if (!movies || movies.length === 0) return null;
   return (
-    <section className='px-6 overflow-x-auto overflow-y-visible'>
+    <section className='hide-scrollbar px-6 overflow-x-auto overflow-y-visible'>
       <h1 className='text-lg md:text-2xl py-4 text-white' aria-label={title}>
         {title}
       </h1>
